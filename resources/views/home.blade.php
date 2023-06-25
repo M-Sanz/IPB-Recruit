408e1b60f81f8c4e0e6704d03c193b160b92b405
maharaihsan <maharaihsan@gmail.com>
2023-06-25 07:12:14 +0700

    fix ipb recruit repository

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+@section('container')
+<h3 class="mt-5 heading1">Organisasi Mahasiswa <br> Ilmu Komputer</h3>
+<h2 class="heading2">Gabung organisasi, tingkatkan skill dan perluas  link!</h2>
+<a href="/organisasi" ><button class="button ungu ngembang">Lihat Organisasi</button></a>
+
+
+@endsection
+
